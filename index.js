23475957015eb7af29cf05718469b75b2ca5a413
AisteB <aistebuneviciene@gmail.com>
2022-03-31 19:16:30 +0200

    Voice method

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ const Dog = require('./js/Dog');
 const rexas = new Dog('Rex', 'brown');
 const spaikas = new Dog('Spike', 'white');
 
-console.log(rexas.name);
-console.log(spaikas.legsCount);
+console.log(rexas);
+console.log(spaikas);
+
+spaikas.voice();
+rexas.voice();
+spaikas.voice();
+spaikas.voice();
